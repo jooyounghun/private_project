3d09ef3aee8182b015c4e38c98de9218d155611a
jooyounghun <jymk04130@gmail.com>
2020-10-22 17:28:49 +0900

    full stack first commit

---
+++ backend/tongtong/src/main/java/com/example/demo/TongtongApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TongtongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongtongApplication.class, args);
+	}
+
+}
